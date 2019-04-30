cafc0f62e98ba440f1fade0378b5f68c47d343eb
Leigh-Ann Crivella <la.crivella.2@gmail.com>
2019-04-29 20:17:53 -0700

    added configs in test files, finished css

---
+++ test/index.js
@@ -0,0 +1,12 @@
+/* eslint-disable */
+
+const jsdom = require('jsdom');
+const { JSDOM } = jsdom;
+const { window } = new JSDOM(``);
+global.window = window;
+global.document = window.document;
+global.FormData = window.FormData;
+global.localStorage = window.localStorage;
+
+require = require('esm')(module);
+module.exports = require('./tests.js');

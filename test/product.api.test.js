5205d948020a8aaec197baae928a9a99e660ad8c
Leigh-Ann Crivella <la.crivella.2@gmail.com>
2019-04-30 16:15:19 -0700

    setting up test

--- test/product.api.test.js
+++ test/product.api.test.js
@@ -1,15 +1,26 @@
 const test = QUnit.test;
-
 QUnit.module('product api');
+
+//function
+const productApi = {
+    save() {
+        //serialize JSON
+        //save to local storage
+    },
+    get() {
+        //get from local storage
+        //deserialize
+        //return
+    }
+};
+
 
 test('creates product api test', (assert) => {
     // Arrange
     // Set up your paramenters and expectations
-    const product = {
+    const product = { name: 'test'};
 
-    };
-
-    // Act
+     // Act
     // Call the function you're testing and set the result to a const
 
     // Assert

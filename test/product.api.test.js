00246c11687eb6f571aeb2adeed0350e91a999dc
Leigh-Ann Crivella <la.crivella.2@gmail.com>
2019-04-30 15:57:46 -0700

    created new test.js files for get-product and product api, and imported those files into tests.js

---
+++ test/product.api.test.js
@@ -0,0 +1,17 @@
+const test = QUnit.test;
+
+QUnit.module('product api');
+
+test('creates product api test', (assert) => {
+    // Arrange
+    // Set up your paramenters and expectations
+    const product = {
+
+    };
+
+    // Act
+    // Call the function you're testing and set the result to a const
+
+    // Assert
+    assert.deepEqual(result, product);
+});

e93738eb6d00f5424d8ddb6c86ad2b7e70704f82
Leigh-Ann Crivella <la.crivella.2@gmail.com>
2019-04-29 20:29:54 -0700

    wired up event listener, tested with console.log

---
+++ src/app.js
@@ -0,0 +1,8 @@
+// selecting the correct nodes
+const pokedex = document.getElementById('pokedex-info');
+
+// adding event listeners
+pokedex.addEventListener('submit', (event) => {
+    event.preventDefault();
+    console.log('hi hi hi');
+});

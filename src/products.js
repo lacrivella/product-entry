e3efc774e5446b0bc1192396c123f5ceb79cd2c0
Leigh-Ann Crivella <la.crivella.2@gmail.com>
2019-05-02 08:46:17 -0700

    more styling in css. created products.js and commented out what needed to be done for the lab

---
+++ src/products.js
@@ -0,0 +1,10 @@
+// import your API object
+
+// reference the table body DOM element
+
+// Get the applicants from the API object
+
+// Loop the list of applicants
+
+// For each applicant, construct a table row and append to the table body
+
